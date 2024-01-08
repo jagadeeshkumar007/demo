33ffc65e078bd42498db617dc30bc238ac793f96
Jagadeesh Kumar <107942144+jagadeeshkumar007@users.noreply.github.com>
2024-01-08 12:11:00 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,13 +1,13 @@
 import streamlit as st
-st.set_page_config(page_title='Cats')
-st.header('Types of Cats')
+st.set_page_config(page_title='Hemanth')
+st.header('Types of Hemanths')
 
 col1, col2 = st.columns(2)
 with col1:
-  st.subheader("Persian Cat")
-  st.image("Screenshot 2023-09-06 155930.png",caption="Persian Cat",width=300,use_column_width=True)
-  st.write("Persian cats are cute")
+  st.subheader("Persian Hemanth")
+  st.image("Screenshot 2023-09-06 155930.png",caption="Persian Hemanth",width=300,use_column_width=True)
+  st.write("Persian hemanth is Rocking")
 with col2:
-  st.subheader("Ragdoll Cat")
-  st.image("Screenshot 2023-09-06 160040.png",caption="Ragdoll Cat",width=300,use_column_width=True)
-  st.write("Ragdoll cats are proud")
+  st.subheader("Ragdoll Hemanth")
+  st.image("Screenshot 2023-09-06 160040.png",caption="Ragdoll Hemanth",width=300,use_column_width=True)
+  st.write("Ragdoll hemanth is pirate")
